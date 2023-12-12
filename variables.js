1a13ae47098063346c7c433a6644fbf047bf87b1
Jamal Buckley <jaybxx94@gmail.com>
2023-12-11 18:23:28 -0800

    Finished assessment 1

---
+++ variables.js
@@ -0,0 +1,45 @@
+//////////////////PROBLEM 1////////////////////
+/* Create a variable called 'myName' with a value that is your name as a string. */
+
+//CODE HERE 
+const myName = "Jamal"
+console.log(myName)
+//////////////////PROBLEM 2////////////////////
+/* Create a variable called 'faveNum' with a value that is your favorite number. */
+
+//CODE HERE
+const faveNum = 2
+console.log(faveNum)
+//////////////////PROBLEM 3////////////////////
+/* Create a variable called 'lovesCode' and assign it a boolean value. */
+
+//CODE HERE
+const lovesCode = true;
+if (lovesCode) {
+  console.log("That's exciting!!");
+} else {
+  console.log("Try again")
+};
+//////////////////PROBLEM 4////////////////////
+/*
+  Write an if-statement that checks if the faveNum variable is 
+  equal to 13. If it is, console.log('lucky 13!'). 
+  If it's not, console.log('not lucky 13').
+*/
+
+//CODE HERE
+if (faveNum === 13){
+  console.log('lucky 13!');
+} else {
+  console.log('not lucky 13');
+}
+
+//////////////////PROBLEM 5////////////////////
+/*
+  Write a for loop that prints out the numbers from 1 through 5.
+*/
+
+//CODE HERE
+for (let i = 1; i < 6; i++) {
+  console.log(i)
+};
